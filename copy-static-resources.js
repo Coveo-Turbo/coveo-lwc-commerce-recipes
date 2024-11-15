62c739a3efd9f866409dc25e387b4a0108f80ebc
Jean-François Allaire <jfallaire@coveo.com>
2024-11-14 22:30:31 -0500

    feat: remove unused static resources for DOMPurify and Marked from project

--- copy-static-resources.js
+++ copy-static-resources.js
@@ -15,55 +15,11 @@ const main = async () => {
   console.info('Begin copy static resources');
   await copyHeadless();
   await copyBueno();
-  await copyMarked();
-  await copyDompurify();
-};
-
-const copyDompurify = async () => {
-  console.info('Begin copy DOMPurify.');
-
-  await mkdir('./force-app/main/default/staticresources/dompurify', {
-    recursive: true,
-  });
-
-  await copy(
-    './node_modules/dompurify/dist/purify.min.js',
-    './force-app/main/default/staticresources/dompurify/purify.min.js'
-  );
-
-  console.info('DOMPurify copied.');
-};
-
-const copyMarked = async () => {
-  console.info('Begin copy Marked.');
-
-  await mkdir('./force-app/main/default/staticresources/marked', {
-    recursive: true,
-  });
-
-  await copy(
-    './node_modules/marked/marked.min.js',
-    './force-app/main/default/staticresources/marked/marked.min.js'
-  );
-
-  console.info('Marked copied.');
 };
 
 const copyHeadless = async () => {
   console.info('Begin copy Headless.');
 
-  await mkdir(
-    './force-app/main/default/staticresources/coveoheadless/case-assist',
-    {recursive: true}
-  );
-  await mkdir(
-    './force-app/main/default/staticresources/coveoheadless/insight',
-    {recursive: true}
-  );
-  await mkdir(
-    './force-app/main/default/staticresources/coveoheadless/recommendation',
-    {recursive: true}
-  );
   await mkdir(
     './force-app/main/default/staticresources/coveoheadless/commerce',
     {recursive: true}
@@ -71,22 +27,6 @@ const copyHeadless = async () => {
   await mkdir(
     './force-app/main/default/staticresources/coveoheadless/definitions/',
     {recursive: true}
-  );
-  await copy(
-    './node_modules/@coveo/headless/dist/quantic/headless.js',
-    './force-app/main/default/staticresources/coveoheadless/headless.js'
-  );
-  await copy(
-    './node_modules/@coveo/headless/dist/quantic/case-assist/headless.js',
-    './force-app/main/default/staticresources/coveoheadless/case-assist/headless.js'
-  );
-  await copy(
-    './node_modules/@coveo/headless/dist/quantic/insight/headless.js',
-    './force-app/main/default/staticresources/coveoheadless/insight/headless.js'
-  );
-  await copy(
-    './node_modules/@coveo/headless/dist/quantic/recommendation/headless.js',
-    './force-app/main/default/staticresources/coveoheadless/recommendation/headless.js'
   );
 
   //TODO: Remove this once the commerce bundle is available in the quantic folder
